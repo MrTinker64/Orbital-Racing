33f4224244cb69e7337ef48d4a842ed78ddac8ec
MrTinker64 <avi.micah@gmail.com>
2023-11-04 16:17:31 -0700

    Renamed file

--- Ship.cs
+++ Ship.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class ShipScript : MonoBehaviour
 {
+    private GameObject planet;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // ! Not sure if this finds the closest
+        planet = GameObject.FindGameObjectWithTag("Planet");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
