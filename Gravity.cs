9ca5105d41557ea686aab7f4d86efa78ddd4cab3
MrTinker64 <avi.micah@gmail.com>
2023-11-05 09:51:07 -0800

    Removed unecessary logging info

--- Gravity.cs
+++ Gravity.cs
@@ -8,8 +8,6 @@ public static class Gravity
 
     public static Vector2 Force(Vector3 pos1, Vector3 pos2, float m1, float m2)
     {
-        Debug.Log("pos1: " + pos1 + ", pos2: " + pos2 + ", m1:" + m1 + ", m2:" + m2);
-
         Vector2 directionToPlanet = pos2 - pos1;
         float distanceSquared = directionToPlanet.sqrMagnitude;
         float gravityMagnitude = G * (m1 * m2) / distanceSquared;
