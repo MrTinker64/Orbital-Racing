cb965803c04ee40f27ddfe3b7a8d6e8aa75dc2ac
MrTinker64 <avi.micah@gmail.com>
2023-11-04 17:13:16 -0700

    first attempt at gravity ! doesn't work

--- Gravity.cs
+++ Gravity.cs
@@ -6,8 +6,10 @@ public static class Gravity
 {
     public const float G = 6.674f;
 
-    public static Vector2 GravityForce(Vector3 pos1, Vector3 pos2, float m1, float m2)
+    public static Vector2 Force(Vector3 pos1, Vector3 pos2, float m1, float m2)
     {
+        Debug.Log("pos1: " + pos1 + ", pos2: " + pos2 + ", m1:" + m1 + ", m2:" + m2);
+
         Vector2 directionToPlanet = pos2 - pos1;
         float distanceSquared = directionToPlanet.sqrMagnitude;
         float gravityMagnitude = G * (m1 * m2) / distanceSquared;
--- Ship.cs
+++ Ship.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShipScript : MonoBehaviour
+public class Ship : MonoBehaviour
 {
-    private float shipMass;
+    public float shipMass;
+    public Rigidbody2D shipRB;
 
     private Planet planet;
     private float planetMass;
+    private Vector3 planetPos;
 
     // Start is called before the first frame update
     void Start()
@@ -15,11 +17,21 @@ public class ShipScript : MonoBehaviour
         // ! Not sure if this finds the closest
         planet = FindObjectOfType<Planet>();
         planetMass = planet.mass;
+        planetPos = planet.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            shipRB.velocity += Vector2.up;
+        }
+    }
 
+    // FixedUpdate is called consistently regardless of framerate
+    void FixedUpdate()
+    {
+        shipRB.AddForce(Gravity.Force(transform.position, planetPos, shipMass, planetMass));
     }
 }
